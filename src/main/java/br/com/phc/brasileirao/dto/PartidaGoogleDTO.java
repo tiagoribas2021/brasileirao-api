c53fe528e4a9e4417583f889ded2d54a0caaef4e
tiagoribas2021 <tiagoribeiroa2o@gmail.com>
2021-04-05 06:28:59 -0300

    Criacao do dto com dados de partida, e criacao do Enuns

---
+++ src/main/java/br/com/phc/brasileirao/dto/PartidaGoogleDTO.java
@@ -0,0 +1,34 @@
+package br.com.phc.brasileirao.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartidaGoogleDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String statusPartida;
+	private String tempoPartida;
+	
+
+	private String nomeEquipeCasa;
+	private String urlLogoEquipeCasa;
+	private Integer placarEquipeCasa;
+	private String golsEquipeCasa;
+	private String placarEstendidoEquipeCasa;
+	
+	private String nomeEquipeVisitante;
+	private String urlLogoEquipeVisitante;
+	private Integer placarEquipeVisitante;
+	private String golsEquipeVisitante;
+	private String placarEstendidoEquipeVisitante;
+	
+}
---
+++ src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
@@ -0,0 +1,16 @@
+package br.com.phc.brasileiraoapi.util;
+
+public class ScrapingUtil {
+
+	private static final String BASE_URL_GOOGLE = "https://www.google.com.br/search?q=";
+	private static final String COMPLEMENTO_URL_GOOGLE = "&HL=PT-br";
+	
+	private static final String BASE_URL_SOFASCORE = "https://www.sofascore.com/";
+	private static final String COMPLEMENTO_URL_SOFASCORE = "";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
